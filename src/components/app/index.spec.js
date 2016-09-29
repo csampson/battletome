7c139c376c09a955cda261f3a0fdcbf0f5f46dfa
Cameron Sampson <csampson.js@gmail.com>
2016-09-29 04:29:38 -0500

    `AppComponent` spec addition + fixed test suite loading

--- src/components/app/index.spec.js
+++ src/components/app/index.spec.js
@@ -1,6 +1,36 @@
 /* eslint-env mocha */
 
-import AppComponent from '.';
+import { Observable } from 'rxjs/Observable';
+import AppComponent   from '.';
+
+const SpellsService = {
+  list: () => (
+    Observable.create(observer => {
+      observer.next([]);
+      observer.complete();
+    })
+  )
+};
+
+let appComponent;
 
 describe('AppComponent', () => {
+  beforeEach(() => {
+    appComponent = new AppComponent(SpellsService);
+  });
+
+  describe('#ngOnInit', () => {
+    beforeEach(() => {
+      sandbox.spy(SpellsService, 'list');
+      appComponent.ngOnInit();
+    });
+
+    it('should fetch a list of spells', () => {
+      SpellsService.list.should.have.been.called;
+    });
+
+    it('should assign spells', () => {
+      appComponent.spells.should.eql([]);
+    });
+  });
 });
