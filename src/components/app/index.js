3330a732884c3d91f275472844300a4f572913eb
Cameron Sampson <csampson.js@gmail.com>
2016-09-28 22:01:09 -0500

    Added background-image

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,16 +2,18 @@
  * @overview Main application container
  * @module   app
  * @requires @angular/core/Component
+ * @requires @angular/core/ViewEncapsulation
  * @requires styles.css
  * @requires template.html
  */
 
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 import styles   from './styles.css';
 import template from './template.html';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app',
   styles: [styles],
   template
