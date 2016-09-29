0dfdb4c35a4b33abebe9bf9429c53799b27ed572
Cameron Sampson <csampson.js@gmail.com>
2016-09-29 18:58:09 -0500

    Swag spec

--- src/services/spells.spec.js
+++ src/services/spells.spec.js
@@ -1,20 +1,30 @@
 /* eslint-env mocha */
 
-import { Observable } from 'rxjs/Observable';
-import SpellsService  from './spells';
+import { Response, ResponseOptions } from '@angular/http';
+import { Observable }                from 'rxjs/Observable';
+import SpellsService                 from './spells';
 
 const responses = {
   okay: Observable.create(observer => {
-    observer.next([]);
+    const responseOptions = new ResponseOptions({
+      body: `[{
+        "name": "<name>",
+        "level": 0
+      }]`
+    });
+
+    const response = new Response(responseOptions);
+
+    observer.next(response);
     observer.complete();
   })
 };
 
-const Http = {
-  get: () => ({
+const Http = sandbox.stub({
+  get: (url) => ({
     map: () => (responses.okay)
   })
-};
+});
 
 let spellsService;
 
@@ -26,6 +36,14 @@ describe('SpellsService', () => {
   describe('#list', () => {
     it('should return an [Observable]', () => {
       spellsService.list().should.be.instanceOf(Observable);
+    });
+
+    it('should deserialize the response', () => {
+      spellsService.list().subscribe(spells => {
+        spells.json().should.eql([
+          { name: '<name>', level: 0 }
+        ]);
+      });
     });
   });
 });
